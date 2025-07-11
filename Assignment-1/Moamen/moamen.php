c48d213bac7df75aed941522c723e0caf784bbaf
moamenFathy <moamnemahmoud118@gmail.com>
2025-07-12 01:52:52 +0300

    php First Program

---
+++ Assignment-1/Moamen/moamen.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  // My First Program In php
+  echo "Hello, World!";
+  /* My First Program In php */
+  ?>
+</body>
+
+</html>
