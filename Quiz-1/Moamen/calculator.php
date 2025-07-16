71654c068f7de379051ffe86e96aefe3a8a0cdb1
moamenFathy <moamnemahmoud118@gmail.com>
2025-07-17 01:39:16 +0300

    quiz 1 submit

---
+++ Quiz-1/Moamen/calculator.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  const num1 = 30;
+  const num2 = 20;
+  $sum = num1 + num2;
+  $sub = num1 - num2;
+  $div = num1 / num2;
+  $mul = num1 * num2;
+  echo "the summation of " . num1 . " and " . num2 . " is $sum <br/>";
+  echo "the subtraction of " . num1 . " and " . num2 . " is $sub <br/>";
+  echo "the division of " . num1 . " and " . num2 . " is $div <br/>";
+  echo "the multiplication of " . num1 . " and " . num2 . " is $mul <br/>";
+  ?>
+</body>
+
+</html>
